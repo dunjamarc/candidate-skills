4ac1c6bf6399a64819b58aa2981e4a32cda9840b
Dunja Markovic <dunjamarkovic86@gmail.com>
2020-02-20 21:16:09 +0100

    Add home page structure

--- src/App/pages/HomePage.js
+++ src/App/pages/HomePage.js
@@ -4,8 +4,12 @@ const HomePage = () => {
     
     return (
         <section className="main">
-            <div className="container">
-                <p>Home Page</p>
+            <div className="container home">
+                <div className="skills-list"></div>
+                <div className="basic-data left"></div>
+                <div className="contact left"></div>
+                <div className="graph left"></div>
+                
             </div>
         </section>
     )
