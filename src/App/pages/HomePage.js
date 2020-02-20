df20fd8848f5137e3ec3812b172be24778e6b1a9
Dunja Markovic <dunjamarkovic86@gmail.com>
2020-02-20 21:41:22 +0100

    Add content to basic info sections

--- src/App/pages/HomePage.js
+++ src/App/pages/HomePage.js
@@ -6,10 +6,24 @@ const HomePage = () => {
         <section className="main">
             <div className="container home">
                 <div className="skills-list"></div>
-                <div className="basic-data left"></div>
-                <div className="contact left"></div>
-                <div className="graph left"></div>
-                
+                <div className="basic-data left">
+                    <img src="https://image.flaticon.com/icons/svg/219/219969.svg" />
+                    <div className="full-name">
+                        <h2>Kristina</h2>
+                        <h2>Milenkovic</h2>
+                    </div>
+                </div>
+                <div className="contact left">
+                    <h3>Contact</h3>
+                    <ul>
+                        <li>kristinamilenkovic@gmail.com</li>
+                        <li>https://www.linkedin.com/in/krismilenkovic/</li>
+                        <li>https://github.com/krismilenkovic/</li>
+                    </ul>
+                </div>
+                <div className="graph left">
+                    <h2>Skill Graph</h2>
+                </div>
             </div>
         </section>
     )
