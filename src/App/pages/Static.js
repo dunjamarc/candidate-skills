5b2f2433e808a4e6ee82a4d1562a7af19b1ee012
Dunja Markovic <dunjamarkovic86@gmail.com>
2020-02-21 22:13:18 +0100

    Style static page

--- src/App/pages/Static.js
+++ src/App/pages/Static.js
@@ -4,8 +4,19 @@ const Static = () => {
     
     return (
         <section className="main">
-            <div className="container">
-                <p>Static</p>
+            <div className="container static">
+                <div className="gallery">
+                    <h1>Gallery</h1>
+                    <img src="https://www.geekfreewebdesign.co.nz/wp-content/uploads/2015/02/business-card-marketing.jpg" alt="gallery" />
+                </div>
+                <div className="text">
+                    <h1>Heading 1</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ac ligula feugiat, pharetra mi ut, aliquet sem. Morbi pellentesque dapibus risus, a condimentum turpis lobortis at. Praesent vitae tempor libero. Nullam nisl sapien, lacinia ac orci sit amet, varius fringilla sapien. In mauris dolor, volutpat ut lorem nec, molestie finibus purus. Etiam ac quam porttitor, commodo risus vel, iaculis est. In tincidunt convallis orci vitae fermentum. Ut sed lectus ornare ipsum eleifend elementum in non ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Curabitur et risus euismod, posuere ante quis, ullamcorper tortor. Cras ut enim consectetur metus tincidunt blandit at eget erat. Suspendisse potenti. Mauris est quam, vehicula eget nisl sodales, volutpat feugiat arcu. Donec ultricies ante et est tincidunt luctus. Donec tristique suscipit scelerisque. Sed ac odio erat.</p>
+                </div>
+                <div className="text">
+                    <h1>Heading 2</h1>
+                    <p>Morbi pretium tristique dapibus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis vitae nibh ut erat hendrerit gravida eget nec magna. Morbi semper metus eget odio eleifend, ac bibendum felis lacinia. Donec convallis eros euismod, vestibulum metus et, bibendum mi. Duis auctor semper ex. Integer tempus eros at massa congue elementum. Suspendisse ornare, felis nec efficitur placerat, risus velit cursus nunc, in luctus purus dui eget neque. Proin dapibus, odio at venenatis interdum, orci lectus convallis lorem, sed accumsan nulla dui vitae odio.</p>
+                </div>
             </div>
         </section>
     )
