5fdba1bfb011af4ea27882b12191ee92155f81d6
Dunja Markovic <dunjamarkovic86@gmail.com>
2020-02-22 14:59:33 +0100

    Add submit form functionality to skill form component

--- src/App/pages/Skills/Skills.js
+++ src/App/pages/Skills/Skills.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import SingleSkill from './HomePage/SingleSkill';
-import skillsData from '../../services/skillsService';
+import SingleSkill from '../HomePage/SingleSkill';
+import skillsData from '../../../services/skillsService';
+import SkillForm from './SkillForm';
 
 class Skills extends Component {
 
@@ -40,30 +41,13 @@ class Skills extends Component {
                             {this.state.allSkills.map(el => <div className="row">
                                 <SingleSkill value={el} key={el.id}/>
                                 <div className="cta">
-                                    <i class="fas fa-edit"></i>
-                                    <i class="fas fa-trash-alt"></i>
+                                    <i className="fas fa-edit"></i>
+                                    <i className="fas fa-trash-alt"></i>
                                 </div>
                             </div>)}
                         </div>
                     </div>
-                    <div className="skill-form">
-                        <h2>Skill Form</h2>
-                        <form>
-                            <div>
-                                <label for="skill-name">Skill Name</label>
-                                <input id="skill-name" type="text"></input>
-                            </div>
-                            <div>
-                                <label for="skill-rating">Skill Rating</label>
-                                <input id="skill-rating" type="number"></input>
-                            </div>
-                            <div>
-                                <label for="skill-year">Skill Year Aquired</label>
-                                <input id="skill-year" type="number"></input>
-                            </div>
-                        </form>
-                        <button>SUBMIT</button>
-                    </div>
+                    <SkillForm />
                 </div>
             </section>
         )
