36b7945df50fb833464984fa785178a17d22fb6a
Dunja Markovic <dunjamarkovic86@gmail.com>
2020-02-22 13:18:22 +0100

    Add skill form to skills page

--- src/App/pages/Skills.js
+++ src/App/pages/Skills.js
@@ -46,7 +46,24 @@ class Skills extends Component {
                             </div>)}
                         </div>
                     </div>
-                    
+                    <div className="skill-form">
+                        <h2>Skill Form</h2>
+                        <form>
+                            <div>
+                                <label for="skill-name">Skill Name</label>
+                                <input id="skill-name" type="text"></input>
+                            </div>
+                            <div>
+                                <label for="skill-rating">Skill Rating</label>
+                                <input id="skill-rating" type="number"></input>
+                            </div>
+                            <div>
+                                <label for="skill-year">Skill Year Aquired</label>
+                                <input id="skill-year" type="number"></input>
+                            </div>
+                        </form>
+                        <button>SUBMIT</button>
+                    </div>
                 </div>
             </section>
         )
