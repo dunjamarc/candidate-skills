fe4ba6283f10ac4af575c4c5beecbb2fb8a6ed1a
Dunja Markovic <dunjamarkovic86@gmail.com>
2020-02-22 22:17:39 +0100

    Add google charts dependency

--- src/App/pages/HomePage/HomePage.js
+++ src/App/pages/HomePage/HomePage.js
@@ -1,7 +1,21 @@
 import React from "react";
 import SkillsList from "./SkillsList";
+import { Chart } from "react-google-charts";
 
 const HomePage = () => {
+
+    const data = [
+        ["Year", "Number of Skills"],
+        ["2016", 7],
+        ["2017", 8],
+        ["2018", 9],
+        ["2019", 9],
+        ["2020", 10],
+    ];
+    const options = {
+        curveType: "function",
+        legend: { position: "bottom" }
+    };
     
     return (
         <section className="main">
@@ -27,6 +41,13 @@ const HomePage = () => {
                 </div>
                 <div className="graph left">
                     <h2>Skill Graph</h2>
+                    <Chart
+                    chartType="LineChart"
+                    width="100%"
+                    height="300px"
+                    data={data}
+                    options={options}
+                    />
                 </div>
             </div>
         </section>
