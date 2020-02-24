e2e60f032cc45c602118324f8d22c61fa81de2aa
Dunja Markovic <dunjamarkovic86@gmail.com>
2020-02-24 22:59:51 +0100

    Toggle class show for mobile menu

--- src/App/partials/Header.js
+++ src/App/partials/Header.js
@@ -1,32 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import {Link} from "react-router-dom";
 
-const Header = (props) => {
+class Header extends Component {
+
+    constructor (props){
+        super(props);
+        this.state = {
+            active: false
+        }
+    }
+
+    addClass = () => {
+        let className = this.state.active;
+        this.setState({
+            active: !className
+        })
+    }
     
-    return (
-        <header>
-            <div className="container">
-                <nav className="nav-wrapper">
-                    <Link to="/" className="brand-logo">LOGO</Link>
+    render () {
+        return (
+            <header>
+                <div className="container">
+                    <nav className="nav-wrapper">
+                        <Link to="/" className="brand-logo">LOGO</Link>
+                        <ul className="menu">
+                            <li><Link to="/">HOME</Link></li>
+                            <li><Link to="/skills">SKILLS</Link></li>
+                            <li><Link to="/static">STATIC</Link></li>
+                        </ul>
+                        <span className="burger" onClick={this.addClass}>
+                            <i className="fa fa-bars"></i>
+                        </span>
+                    </nav>
+                </div>
+                <div className={`mobile-nav ${this.state.active ? "show" : ""}`}>
                     <ul className="menu">
-                        <li><Link to="/">HOME</Link></li>
-                        <li><Link to="/skills">SKILLS</Link></li>
-                        <li><Link to="/static">STATIC</Link></li>
+                        <li><Link to="/" onClick={this.addClass}>HOME</Link></li>
+                        <li><Link to="/skills" onClick={this.addClass}>SKILLS</Link></li>
+                        <li><Link to="/static" onClick={this.addClass}>STATIC</Link></li>
                     </ul>
-                    <span className="burger">
-                        <i className="fa fa-bars"></i>
-                    </span>
-                </nav>
-            </div>
-            <div className="mobile-nav">
-                <ul className="menu">
-                    <li><Link to="/">HOME</Link></li>
-                    <li><Link to="/skills">SKILLS</Link></li>
-                    <li><Link to="/static">STATIC</Link></li>
-                </ul>
-            </div>
-        </header>
-    )
+                </div>
+            </header>
+        )
+    }
 }
 
 export default Header;
